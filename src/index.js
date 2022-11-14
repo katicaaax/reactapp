bafa94dbbeee3ab89724b647ef8e72d3c8e875a1
katicaaax <radosavljevic.katarina98@gmail.com>
2022-11-14 22:06:28 +0100

    Drugi commit. Pravljenje prve komponente.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const prviElement = <h1> Hello world! </h1>
+//const prviElement = <h1> Hello world! </h1>
 
-ReactDOM.render(
-  prviElement,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+  
+
 
